ab1dfd1be9376d90807a6967e3db18e3fc3213ff
Andres Chavez <andres.cz.chavez@gmail.com>
2021-10-20 12:17:53 -0500

    funcionalidad de compartir por redes sociales solo disponible en dispositivos moviles

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
-// import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+
 
 
 @Component({
@@ -11,7 +15,8 @@ import { Article } from '../../interfaces/interfaces';
 export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
-  constructor() {
+  constructor(private iab: InAppBrowser, private actionCtrl: ActionSheetController,
+              private socialsharing: SocialSharing) {
     // console.log('es null noticiia', this.noticia === undefined);
    }
 
@@ -19,7 +24,41 @@ export class NoticiaComponent implements OnInit {
   }
   abrirNoticia(){
     console.log('click noticia');
-    // const browser = this.iab.create(this.noticia.url, '_system');
+    const browser = this.iab.create(this.noticia.url);
+  }
+  async lanzarMenu(){
+    const actionSheet = await this.actionCtrl.create({
+      header: 'Opciones',
+      cssClass: 'myPage',
+      buttons: [{
+        text: 'Favorito',
+        icon: 'heart',
+        cssClass: 'myActionSheetBtnStyle-favorite',
+        handler: () => {
+          console.log('favorite clicked');
+        }
+      }, {
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'myActionSheetBtnStyle-Compartir',
+        handler: () => {
+          this.socialsharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url
+          );
+        }
+      },{
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
